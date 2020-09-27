ba8092b50a71dc5fb90e827c8f69ded074754163
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-09-27 13:19:55 +0900

    complete assignmnet 4

--- personal_assignment/assignment_4/Lab4-201420979/Lab4.js
+++ personal_assignment/assignment_4/Lab4-201420979/Lab4.js
@@ -1,14 +1,25 @@
 const btnContainer = document.querySelector(".btn");
 const textAreaContainer = document.querySelector(".textArea");
 const sectionContainer = document.querySelector("#lab3Section");
-const boxContainer = document.querySelector(".box1");
+const cntDivContainer = document.querySelector(".cntDiv");
 
 let degreeOfText = 0;
 
+function refreshDegreeOfMemo() {
+    degreeOfText++;
+    cntDivContainer.innerHTML = degreeOfText;
+}
+
 function addArticle(inputStr) {
     let creatingArticle = document.createElement("article");
+
     creatingArticle.innerHTML = inputStr;
+    creatingArticle.style.backgroundColor = "green";
+    creatingArticle.style.marginBottom = 10 + "px";
+    creatingArticle.style.width = 100 + "%";
+
     sectionContainer.appendChild(creatingArticle);
+    refreshDegreeOfMemo();
 };
 
 function addTextHandler(event) {
@@ -21,6 +32,8 @@ function addTextHandler(event) {
 
 function init() {
     btnContainer.addEventListener("click", addTextHandler);
+    cntDivContainer.innerHTML = degreeOfText;
+
 };
 
 init();
