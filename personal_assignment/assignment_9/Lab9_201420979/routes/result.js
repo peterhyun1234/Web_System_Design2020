7985f29a4fd399aa45f5c3fc996a69652c02fdfa
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-11-03 23:50:15 +0900

    add router for result

--- personal_assignment/assignment_9/Lab9_201420979/routes/result.js
+++ personal_assignment/assignment_9/Lab9_201420979/routes/result.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
+    console.log(res);
     res.render('result', { title: 'Result' });
 });
 
