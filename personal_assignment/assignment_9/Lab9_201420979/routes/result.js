ccd01923eb97e1a01cb9b4c47504a8e8b5864f34
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-11-04 19:20:06 +0900

    complete assignmnet 9

--- personal_assignment/assignment_9/Lab9_201420979/routes/result.js
+++ personal_assignment/assignment_9/Lab9_201420979/routes/result.js
@@ -3,19 +3,30 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-    //console.log(res);
-    let recv_movie_title = "Begin Again";
-    let recv_movie_cast = "Mark Ruffalo, Keira Knightley";
-    let recv_movie_genre = "Melodrama";
-    let recv_poster_location = "";
-    let recv_stars = 3;
-    res.render('result', {
-        movie_title: recv_movie_title,
-        movie_cast: recv_movie_cast,
-        movie_genre: recv_movie_genre,
-        poster_location: recv_poster_location,
-        stars: recv_stars
-    });
+    let isPossible = true;
+
+    console.log(req.body);
+
+    let recv_movie_title = req.body.movie_title;
+    let recv_movie_cast = req.body.movie_cast;
+    let recv_movie_genre = req.body.movie_genre;
+    let recv_poster_location = req.body.poster_location;
+    let recv_stars = req.body.rank;
+
+    if (recv_movie_title == "" || recv_movie_cast == "" || recv_movie_genre == "" || recv_poster_location == "") isPossible = false;
+    if (isPossible) {
+        res.render('result', {
+            movie_title: recv_movie_title,
+            movie_cast: recv_movie_cast,
+            movie_genre: recv_movie_genre,
+            poster_location: recv_poster_location,
+            stars: recv_stars
+        });
+    } else {
+        // 하나라도 누락되거나 경로가 잘못된 경우
+        let err = "하나라도 누락되거나 경로가 잘못된 경우";
+        res.render('error', { error: err });
+    }
 });
 
 module.exports = router;
