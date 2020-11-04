69a29c1e6cfa4af5240798aa59d2e38332a5a270
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-11-04 18:37:47 +0900

    add some post func

--- personal_assignment/assignment_9/Lab9_201420979/routes/result.js
+++ personal_assignment/assignment_9/Lab9_201420979/routes/result.js
@@ -3,8 +3,19 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-    console.log(res);
-    res.render('result', { title: 'Result' });
+    //console.log(res);
+    let recv_movie_title = "Begin Again";
+    let recv_movie_cast = "Mark Ruffalo, Keira Knightley";
+    let recv_movie_genre = "Melodrama";
+    let recv_poster_location = "";
+    let recv_stars = 3;
+    res.render('result', {
+        movie_title: recv_movie_title,
+        movie_cast: recv_movie_cast,
+        movie_genre: recv_movie_genre,
+        poster_location: recv_poster_location,
+        stars: recv_stars
+    });
 });
 
 module.exports = router;
